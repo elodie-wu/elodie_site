b142fbee5f1f327431d1361834b1f04b53d50cee
Elodie Wu <elodie.wu@outlook.com>
2026-09-15 22:03:29 +1200

    Update work page

--- src/pages/WorkPage.tsx
+++ src/pages/WorkPage.tsx
@@ -1,5 +1,5 @@
 import { ScenePlaceholderPage } from '../components/ScenePlaceholderPage'
 
 export function WorkPage() {
-  return <ScenePlaceholderPage background="work-bg.png" pageName="Work" bottomFade />
+  return <ScenePlaceholderPage background="work-bg-v2.png" pageName="Work" bottomFade />
 }
